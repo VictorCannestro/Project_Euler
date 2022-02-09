eb25758e182ea1c4360eb25cf6c5bda23616b88e
VictorCannestro <vcannestro@gmail.com>
2022-02-08 18:51:25 -0800

    better tests on p1

--- tests/test_pe1.py
+++ tests/test_pe1.py
@@ -1,14 +1,15 @@
 from src.pe1 import multiples
 
 
-def test_type():
-    assert type(multiples(n=3)) == list
+class TestMultiples(object):
+    def test_type(self):
+        assert type(multiples(n=3)) == list
+        
+    def test_empty(self):
+        assert sum(multiples(n=0)) == 0    
     
-def test_empty():
-    assert sum(multiples(n=0)) == 0    
-
-def test_simple():
-    assert sum(multiples(n=6)) == 8
-
-def test_ans():
-    assert sum(multiples(n=1000)) == 233168
+    def test_simple(self):
+        assert sum(multiples(n=6)) == 8
+    
+    def test_ans(self):
+        assert sum(multiples(n=1000)) == 233168
