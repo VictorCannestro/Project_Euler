b2db1fbfd8a293b6b44f1f49446b802a5c2c428d
VictorCannestro <vcannestro@gmail.com>
2022-02-10 15:07:31 -0800

    better test on pe5, 14

--- src/pe5.py
+++ src/pe5.py
@@ -7,15 +7,31 @@
 #
 # What is the smallest positive number that is evenly divisible 
 # by all of the numbers from 1 to 20?
-#
+
 # Ans: 232792560
 ############################################################################################################################
+import math 
 
-def bruteForce(d: int) -> int:
+
+def prime_factors_set(n: int) -> set:
+        '''Utillity function to get the prime factor decomposition of n'''
+        factors = set()
+        while n % 2 == 0:
+            factors.update([2])
+            n = n / 2   
+        for i in range(3, int(math.sqrt(n))+1, 2): # n became odd
+            while (n % i == 0):
+                factors.update([i])
+                n = n / i
+        if n > 2:
+            factors.update([int(n)])
+        return factors 
+    
+    
+def brute_force(d: int) -> int:
     '''
     Args:
-        d (int > 0): the upper bound of the consecutive divisors
-    
+        d (int > 0): the upper bound of the consecutive divisors  
     Returns:
         (int): the smallest positive number that is evenly divisible 
                by all of the numbers from 1 to d
@@ -24,16 +40,25 @@ def bruteForce(d: int) -> int:
         raise ValueError("Must input a positive integer.")
     elif d == 1:
         return 1
-    smallest = d*(d-1)
+    smallest = d * (d-1)
     # A generator comprehension that checks if smallest divides the numbers 
     # from 1 to d without any remainder. Contains boolean values. 
     while sum(smallest % i == 0 for i in range(1, d+1)) != d:
         smallest += 1
-    return smallest
+    return smallest   
 
-#def smallestEvenlyDivisible(d: int) -> int:
-
+def smallest_evenly_divisible(d: int) -> int:
+    if d < 1:
+        raise ValueError("Must input a positive integer.")
+    elif d == 1:
+        return 1
+    else:
+        calc = smallest_evenly_divisible(d-1)
+        if calc % d == 0:
+            return calc
+        else:
+            return calc * min(prime_factors_set(d))
 
 
 if __name__ == "__main__":
-    print(bruteForce(20))
+    print(smallest_evenly_divisible(20))
--- tests/test_pe5.py
+++ tests/test_pe5.py
@@ -1,29 +1,68 @@
-from src.pe5 import bruteForce
+from src.pe5 import brute_force, smallest_evenly_divisible, prime_factors_set
 import pytest 
 
 
-class TestSmallestMult(object):
+class TestPrimeFactorsSet(object):
+    xs = [2, 3, 4, 10, 100]
+    answers = [{2}, {3}, {2}, {2,5}, {2,5}]
+    
+    def test_type(self):
+        calc = type(prime_factors_set(6))
+        ans = set
+        assert calc == ans, f"Expected {ans} but got {calc}"
+        
+    @pytest.mark.parametrize("x, ans", zip(xs, answers))
+    def test_standard(self, x, ans):
+        calc = prime_factors_set(x)
+        message = f"Expected {ans} but got {calc}"
+        assert calc == ans, message
+
+
+class TestBruteForce(object):
     xs = [1, 2, 3, 4, 5, 10]
     answers = [1, 2, 6, 12, 60, 2520]
     
     def test_type(self):
-        calc = type(bruteForce(1))
+        calc = type(brute_force(1))
         ans = int
         assert calc == ans, f"Expected {ans} but got {calc}"
         
     @pytest.mark.xfail
     def test_negative(self):
         with pytest.raises(ValueError) as exception_info: # store the exception
-            fail = bruteForce(-1)
+            fail = brute_force(-1)
         assert exception_info.match("Input must be non-negative")
     
     @pytest.mark.parametrize("x, ans", zip(xs, answers))
     def test_standard(self, x, ans):
-        message = f"Not the smallest number that can be divided by each of the numbers from 1 to {x} without any remainder"
-        assert bruteForce(1) == 1, message
+        calc = brute_force(x)
+        message = f"Expected {ans} but got {calc}"
+        assert calc == ans, message
+        
+        
+class TestSmallestMult(object):
+    xs = [1, 2, 3, 4, 5, 10]
+    answers = [1, 2, 6, 12, 60, 2520]
+    
+    def test_type(self):
+        calc = type(smallest_evenly_divisible(1))
+        ans = int
+        assert calc == ans, f"Expected {ans} but got {calc}"
+        
+    @pytest.mark.xfail
+    def test_negative(self):
+        with pytest.raises(ValueError) as exception_info: # store the exception
+            fail = smallest_evenly_divisible(-1)
+        assert exception_info.match("Input must be non-negative")
+    
+    @pytest.mark.parametrize("x, ans", zip(xs, answers))
+    def test_standard(self, x, ans):
+        calc = smallest_evenly_divisible(x)
+        message = f"Expected {ans} but got {calc}"
+        assert calc == ans, message
         
     def test_ans(self):
-        calc = bruteForce(20)
+        calc = smallest_evenly_divisible(20)
         ans = 232792560
         message = f"Expected {ans} but got {calc}"
         assert ans == calc, message
