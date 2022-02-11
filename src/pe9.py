0a98403f20cb486059c52e710d7a6dabfb949b8e
VictorCannestro <vcannestro@gmail.com>
2022-02-11 15:21:49 -0800

    better tests p9

--- src/pe9.py
+++ src/pe9.py
@@ -9,9 +9,11 @@
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+#
+# Ans: 31875000
 ############################################################################################################################
 
-def findTriplets(n):
+def findTriplets(n: int) -> tuple:
     '''
     Args:
         n (int >= 12): the sum of pythagorean triplets
@@ -22,16 +24,19 @@ def findTriplets(n):
     Notes:
         We use the fact that c = n - a - b and a < b < c.
     '''
-    ts =  [(a, b, n-a-b) for a in range(1,n) for b in range(a,n) if (a**2 + b**2 - (n-a-b)**2 == 0)]
-    return ts[0]
+    for a in range(1,n):
+        for b in range(a,n):
+            if (a**2 + b**2 - (n-a-b)**2 == 0):
+                return (a, b, n-a-b)    
+    raise ValueError(f"No triplets found that sum to {n}.")
 
-def product(ts):
+def product(triplets: tuple) -> int:
+    '''Utility function'''
     prod = 1
-    for i in ts:
+    for i in triplets:
         prod *= i
     return prod
 
+
 if __name__ == "__main__":
-    n = 1000
-    triplets = findTriplets(n)
-    print(product(triplets))
+    print(product(findTriplets(1000)))
--- tests/test_pe9.py
+++ tests/test_pe9.py
@@ -1,8 +1,38 @@
 from src.pe9 import findTriplets, product
+import pytest
 
 
-def test_1():
-    assert findTriplets(12) == (3,4,5)
+class TestFindTriplets(object):
+    def test_given(self):
+        calc = findTriplets(12)
+        ans = (3,4,5)
+        message =  f"Expected {ans}\nGot {calc}"
+        assert calc == ans, message
+        
+    @pytest.mark.xfail
+    def test_empty(self):
+        with pytest.raises(ValueError) as exception_info:
+            fail = findTriplets(50)
+        assert exception_info.match("No triplets found that sum to 50.")
+
+
+class TestProduct(object):
+    def test_given(self):
+        calc = product((3,4,5))
+        ans = 60
+        message =  f"Expected {ans}\nGot {calc}"
+        assert calc == ans, message  
+
+
+class TestFunctional(object):
+    def test_given(self):
+        calc = product(findTriplets(12))
+        ans = product((3,4,5))
+        message =  f"Expected {ans}\nGot {calc}"
+        assert calc == ans, message
     
-def test_2():
-    assert product(findTriplets(12)) == 3*4*5
+    def test_answer(self):
+        calc = product(findTriplets(1000))
+        ans = 31875000
+        message =  f"Expected {ans}\nGot {calc}"
+        assert calc == ans, message
