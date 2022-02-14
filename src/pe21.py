66ea2da4e74837516fdce396e11326e61bd18732
VictorCannestro <vcannestro@gmail.com>
2022-02-14 08:43:15 -0800

    better tests on pe21

--- src/pe21.py
+++ src/pe21.py
@@ -8,52 +8,47 @@
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. 
 # The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 #
-# Evaluate the sum of all the amicable numbers under 10000.
+# Evaluate the sum of all the amicable numbers under 10,000.
 #
-# Ans: 31626
+# Ans: 31626. And the answer for all under 100,000 is 852810.
 ############################################################################################################################
 import numpy as np
 
 
-def d(n):
+def d(n: int) -> int:
     '''Amicable number: the sum of the proper divisors of n'''
     return np.sum(propDivisors(n))
 
-def propDivisors(n):
+def propDivisors(n: int) -> np.array:
     '''
     Args: 
             n (int): the number to find divisors of
-    
     Notes:
             Proper divisors of n: numbers less than n which divide evenly into n
             - n != x ensures the divisor isn't the number itself
             - n % x == 0 ensures n divides x
             - range(1, (n + 1) // 2 + 1) to cut down the search space 
-            
-            A simplified solution suitible for small numbers
-            http://rosettacode.org/wiki/Proper_divisors#Python
     '''
     return np.array([x for x in range(1, (n + 1) // 2 + 1) if n % x == 0 and n != x])
     
-def isAmicablePair(n,m):
+def isAmicablePair(n: int, m: int) -> bool:
     '''
     Amicable pair: If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and 
                    each of a and b are called amicable numbers.
     '''
     return  d(n) == m and d(m) == n
 
-def getAmicableParis(k):
+# IS A BOTTLENECK!
+def getAmicableParis(k: int):
     '''Generate the amicable paris found in range [1,k]'''
-    # Make a dictionary of all propor divisor sums
-    candidates = {n: d(n) for n in range(1, k + 1)}
+    candidates = {n: d(n) for n in range(1, k + 1)}    # Make a dictionary of all propor divisor sums
     for n, dn in candidates.items():
-        # Check the conditions for an amicable pair
-        if n < dn and dn <= k and candidates[dn] == n:
+        if n < dn and dn <= k and candidates[dn] == n: # Check the conditions for an amicable pair
             yield n, dn
             
-
+def calculateSumOfAmicables(N:int=10000) -> int:
+    return sum(p[0]+p[1] for p in getAmicableParis(N))
+    
+    
 if __name__ == "__main__":
-    N = 10000
-    pairs = [*getAmicableParis(N)]
-    result = sum(p[0]+p[1] for p in pairs)
-    print(result)
+    print(calculateSumOfAmicables())
--- tests/test_pe21.py
+++ tests/test_pe21.py
@@ -1,14 +1,55 @@
 import numpy as np
-from src.pe21 import propDivisors, d
-
-
-def test_1():
-    tol = 10e-5
-    calc = propDivisors(284)
-    ans = np.array([1, 2, 4, 71,142])
-    assert sum(np.abs(calc - ans)) < tol, f"Expected {ans}\nGot {calc}"
+from src.pe21 import propDivisors, d, isAmicablePair, getAmicableParis, calculateSumOfAmicables
     
-def test_2():
-    calc = d(284)
-    ans = 220
-    assert calc == ans, f"Expected {ans}\nGot {calc}"
+    
+class TestD(object):
+    def test_given(self):
+        calc = d(284)
+        ans = 220
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message        
+    
+
+class TestPropDivisors(object):
+    def test_given(self):
+        tol = 10e-5
+        calc = propDivisors(284)
+        ans = np.array([1, 2, 4, 71, 142])
+        message = f"Expected {ans}\nBut got {calc}"
+        assert sum(np.abs(calc - ans)) < tol, message   
+ 
+
+class TestIsAmicablePair(object):
+    def test_given(self):
+        calc = isAmicablePair(284,220)
+        ans = True
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message 
+
+    def test_false(self):
+        calc = isAmicablePair(2,3)
+        ans = False
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message 
+        
+        
+class TestGetAmicablePairs(object):
+    def test_low_threshold(self):
+        calc = [*getAmicableParis(10)]
+        ans = []
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message 
+    
+    def test_smallest_pair(self):
+        calc = [*getAmicableParis(300)]
+        ans = [(220, 284)]
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message 
+        
+        
+class TestCalculateSumOfAmicables(object):
+    def test_answer(self):
+        calc = calculateSumOfAmicables()
+        ans = 31626
+        message = f"Expected {ans}\nBut got {calc}"
+        assert calc == ans, message
